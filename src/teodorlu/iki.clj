c6214a727de4440a3591ba503551dc1495cb54c2
Teodor Heggelund <git@teod.eu>
2023-01-29 12:15:54 +0100

    CLI outline

--- src/teodorlu/iki.clj
+++ src/teodorlu/iki.clj
@@ -1,4 +1,24 @@
-(ns teodorlu.iki)
+(ns teodorlu.iki
+  (:require
+   [babashka.cli :as cli]
+   [clojure.string :as str]))
 
-(defn -main [& _args]
-  (println "Hello, IKI!"))
+(defn list-pages [{}])
+
+(declare dispatch-table)
+
+(defn print-subcommands [{}]
+  (println "usage: neil-quickadd <command>")
+  (println "")
+  (println "available commands:")
+  (doseq [{:keys [cmds helptext]} dispatch-table]
+    (let [helptext (if helptext (str "     ; " helptext)
+                       "")]
+      (println (str "  " "neil-quickadd " (str/join " " cmds) helptext)))))
+
+(def dispatch-table
+  [{:cmds ["list-pages"] :fn list-pages}
+   {:cmds ["help"]       :fn print-subcommands :helptext "Get help!"}])
+
+(defn -main [& args]
+  (cli/dispatch dispatch-table args))
